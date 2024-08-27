354267e19f86bc07e27dd73d95e07c9ce8614005
srikanth itharaju <itharajusrikanth53@gmail.com>
2024-08-27 17:38:15 +0530

    helpers

--- IJPMvcApp/Models/Helper.cs
+++ IJPMvcApp/Models/Helper.cs
@@ -6,10 +6,21 @@ namespace IJPMvcApp.Models
     {
         public static async Task<List<SelectListItem>> GetStatus()
         {
-            List<SelectListItem> status = new List<SelectListItem>();
-           
-            
-              return status;
+             List<SelectListItem> status = new List<SelectListItem>();
+
+             status.Add(new SelectListItem { Text = "Reviewing", Value = "Reviewing" });
+             status.Add(new SelectListItem { Text = "Accepted", Value = "Accepted" });
+             status.Add(new SelectListItem { Text = "Rejected", Value = "Rejected" });
+             return status;
+        }
+        public static async Task<List<SelectListItem>> GetLevel()
+        {
+            List<SelectListItem> level = new List<SelectListItem>();
+
+            level.Add(new SelectListItem { Text = "B", Value = "B" });
+            level.Add(new SelectListItem { Text = "I", Value = "I" });
+            level.Add(new SelectListItem { Text = "A", Value = "A" });
+            return level;
         }
     }
 }
