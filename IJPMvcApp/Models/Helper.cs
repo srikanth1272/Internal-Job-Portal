46e76edca8527a3911b0136747b23965af27604e
srikanth itharaju <itharajusrikanth53@gmail.com>
2024-08-27 16:07:04 +0530

    final mvc

---
+++ IJPMvcApp/Models/Helper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IJPMvcApp.Models
+{
+    public class Helper
+    {
+        public static async Task<List<SelectListItem>> GetStatus()
+        {
+            List<SelectListItem> status = new List<SelectListItem>();
+           
+            
+              return status;
+        }
+    }
+}
--- JobWebApi/Controllers/JobController.cs
+++ JobWebApi/Controllers/JobController.cs
@@ -9,7 +9,6 @@ namespace JobWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class JobController : ControllerBase
     {
         IJobRepoAsync repo;
