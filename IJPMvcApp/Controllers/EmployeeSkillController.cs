aab0dd0c303afe2f139b1c01265852e36a73f887
Vidhya-reddy <morapellyvidhya22@gmail.com>
2024-08-27 14:19:16 +0530

    Added ImageHelper

--- IJPMvcApp/Controllers/EmployeeSkillController.cs
+++ IJPMvcApp/Controllers/EmployeeSkillController.cs
@@ -1,9 +1,11 @@
 ﻿using IJPMvcApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IJPMvcApp.Controllers
 {
+    
     public class EmployeeSkillController : Controller
     {
         // GET: EmployeeSkillController
