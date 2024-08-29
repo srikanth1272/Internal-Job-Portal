3aea671ce19f5c41a88ce48a3c09068f9f40969a
srikanth itharaju <itharajusrikanth53@gmail.com>
2024-08-29 12:02:09 +0530

    final

--- IJPMvcApp/Controllers/HomeController.cs
+++ IJPMvcApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using IJPMvcApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace IJPMvcApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
